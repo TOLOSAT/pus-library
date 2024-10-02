d6331d734c0ce872bda9dacf41ddd7a0a678ac1c
Merlin Kooshmanian <mkooshmanian@gmail.com>
2024-10-02 13:25:33 +0200

    Updated linkerscript with bss section and removed all IN_{}_SECTION defines in the code

--- pus-library.ld.h
+++ pus-library.ld.h
@@ -1,12 +1,18 @@
 /* PUS contained sections */
 #define PUS_TEXT_SEGMENT    . = ALIGN(4); \
-                            _text_pus_start_ = .; \
+                            _pus_text_start_ = .; \
                             *libpus*.a:*(.text .text.*) \
                             . = ALIGN(4); \
-                            _text_pus_end_ = .;
+                            _pus_text_end_ = .;
 
 #define PUS_DATA_SEGMENT    . = ALIGN(4); \
-                            _data_pus_start_ = .; \
-                            *libpus*.a:*(.data .data.* .bss .bss.*) \
+                            _pus_data_start_ = .; \
+                            *libpus*.a:*(.data .data.*) \
                             . = ALIGN(4); \
-                            _data_pus_end_ = .;
+                            _pus_data_end_ = .;
+
+#define PUS_BSS_SEGMENT    . = ALIGN(4); \
+                            _pus_bss_start_ = .; \
+                            *libpus*.a:*(.bss .bss.*) \
+                            . = ALIGN(4); \
+                            _pus_bss_end_ = .;
