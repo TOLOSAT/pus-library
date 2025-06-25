4e336731285c7b409678e453d68ca1a469351d58
Merlin Kooshmanian <mkooshmanian@gmail.com>
2025-06-25 19:57:20 +0200

    feat: PUS193 init && fix : doc & verif && feat : iridium inst in iridium app && fix : rename InitPus11 into InitS11
    Co-authored-by: Yrolis <Yrolis@users.noreply.github.com>

--- src/services/pus11.c
+++ src/services/pus11.c
@@ -40,12 +40,12 @@ static pus11Context_t *pus11_context_pointer;
 /*************************** Functions Definitions ***************************/
 
 /**
- * @fn          InitPus11(pus11Context_t *pus11_context)
+ * @fn          InitS11(pus11Context_t *pus11_context)
  * @brief       This function init pus 11 files
  * @param[in]   pus11_context PUS11 context used for configuration
  * @retval      #RET_SUCCESSFUL always
  */
-returnCode_t InitPus11(pus11Context_t *pus11_context)
+returnCode_t InitS11(pus11Context_t *pus11_context)
 {
     returnCode_t return_value = RET_SUCCESSFUL;
     length_t file_size        = 0;
