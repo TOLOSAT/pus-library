b6a3fcfd83d6065ea11eb057a77cc6e17faf76ec
Merlin Kooshmanian <mkooshmanian@gmail.com>
2026-03-07 13:51:09 +0100

    chore: Added pus3 API in header && chore: updated ExecuteS3SS5 and ExecuteS3SS6 accordingly

--- src/services/pus3.c
+++ src/services/pus3.c
@@ -16,28 +16,20 @@
 
 /*************************** Functions Declarations **************************/
 
+static returnCode_t GetLinenoFromHKID(pus3HKTable_t *table, pus3HKID_t hkid, length_t *lineno);
+
 /*************************** Variables Definitions ***************************/
 
 /*************************** Functions Definitions ***************************/
 
-/**
- * @fn          BuildS3SS25(pusTM_t *tm, housekeepingReport_t *report)
- * @brief       Function that send S3SS25 TM (housekeeping report)
- * @param[out]  tm TM that will be sent
- * @param[in]   report Housekeeping report
- * @retval      #RET_INVALID_PARAM if a pointer is NULL
- * @retval      #RET_ERROR if cannot build TM
- * @retval      #RET_SUCCESSFUL else
- */
-returnCode_t BuildS3SS25(pusTM_t *tm, housekeepingReport_t *report)
+returnCode_t InitS3(pus3Env_t *pus3_env)
 {
     returnCode_t return_value = RET_SUCCESSFUL;
 
     // Check parameter(s)
-    if ((tm != NULL) && (report != NULL))
+    if (pus3_env != NULL)
     {
-        // Build TM
-        return_value = BuildTM(tm, 3u, 25u, (pusData_t *)report, HOUSEKEEPING_REPORT_SIZE);
+
     }
     else
     {
@@ -63,18 +55,42 @@ returnCode_t ExecuteS3SS5(void *env, pusTC_t *tc, pusTM_t *tm, pusExecutionError
     returnCode_t return_value = RET_SUCCESSFUL;
 
     // Unused Parameters
-    (void)(env);
     (void)(tm);
 
     // Check parameter(s)
-    if ((tc != NULL) && (error_code != NULL))
+    if ((env != NULL) && (tc != NULL) && (error_code != NULL))
     {
         // Error code Initialization
         *error_code = PUS_EXECUTION_NO_ERROR;
 
-        if ((tc->spp_header.packet_data_length + 1u) == (TC_HEADER_SIZE + HOUSEKEEPING_ID_SIZE + CRC_TRAILER_SIZE))
+        if ((tc->spp_header.packet_data_length + 1u) == (TC_HEADER_SIZE + sizeof(pus3HKID_t) + CRC_TRAILER_SIZE))
         {
-            // TO DO : USE HK syscalls when available
+            // Get environment
+            pus3Env_t *pus3_env = (pus3Env_t *)env;
+
+            // Get HKID
+            pus3HKID_t hkid = BIG_ENDIAN_ARRAY_TO_UINT32(tc->data);
+
+            // Look for a specific HK or every HK
+            if (hkid != 0)
+            {
+                length_t lineno = 0;
+                // Search for HK Param
+                return_value = GetLinenoFromHKID(&pus3_env->pus3_hk_table, hkid, &lineno);
+                if (return_value == RET_SUCCESSFUL)
+                {
+                    // Enable the HK
+                    pus3_env->pus3_hk_table.entries[lineno].status = HK_REPORT_ENABLE;
+                }
+            }
+            else
+            {
+                // Enable every HK
+                for (uint32_t i = 0u; i < pus3_env->pus3_hk_table.size; i++)
+                {
+                    pus3_env->pus3_hk_table.entries[i].status = HK_REPORT_ENABLE;
+                }
+            }
         }
         else
         {
@@ -106,18 +122,42 @@ returnCode_t ExecuteS3SS6(void *env, pusTC_t *tc, pusTM_t *tm, pusExecutionError
     returnCode_t return_value = RET_SUCCESSFUL;
 
     // Unused Parameters
-    (void)(env);
     (void)(tm);
 
     // Check parameter(s)
-    if ((tc != NULL) && (error_code != NULL))
+    if ((env != NULL) && (tc != NULL) && (error_code != NULL))
     {
         // Error code Initialization
         *error_code = PUS_EXECUTION_NO_ERROR;
 
-        if ((tc->spp_header.packet_data_length + 1u) == (TC_HEADER_SIZE + HOUSEKEEPING_ID_SIZE + CRC_TRAILER_SIZE))
+        if ((tc->spp_header.packet_data_length + 1u) == (TC_HEADER_SIZE + sizeof(pus3HKID_t) + CRC_TRAILER_SIZE))
         {
-            // TO DO : USE HK syscalls when available
+            // Get environment
+            pus3Env_t *pus3_env = (pus3Env_t *)env;
+
+            // Get HKID
+            pus3HKID_t hkid = BIG_ENDIAN_ARRAY_TO_UINT32(tc->data);
+
+            // Look for a specific HK or every HK
+            if (hkid != 0)
+            {
+                length_t lineno = 0;
+                // Search for HK Param
+                return_value = GetLinenoFromHKID(&pus3_env->pus3_hk_table, hkid, &lineno);
+                if (return_value == RET_SUCCESSFUL)
+                {
+                    // Disable the HK
+                    pus3_env->pus3_hk_table.entries[lineno].status = HK_REPORT_DISABLE;
+                }
+            }
+            else
+            {
+                // Disable every HK
+                for (uint32_t i = 0u; i < pus3_env->pus3_hk_table.size; i++)
+                {
+                    pus3_env->pus3_hk_table.entries[i].status = HK_REPORT_DISABLE;
+                }
+            }
         }
         else
         {
@@ -128,6 +168,45 @@ returnCode_t ExecuteS3SS6(void *env, pusTC_t *tc, pusTM_t *tm, pusExecutionError
     else
     {
         return_value = RET_INVALID_PARAM;
+    }
+
+    return return_value;
+}
+
+/**
+ * @fn          GetLinenoFromHKID(pus3HKTable_t *table, pus3HKID_t hkid, length_t *lineno)
+ * @brief       This function search for an HK param report entry linenoo in hk table with a hkid
+ * @param[in]   table HK table to search in
+ * @param[in]   hkid HK ID to search for
+ * @param[out]  lineno Lineno of the HK param report entry
+ * @retval      #RET_NOT_AVAILABLE if hkid does not exist in table
+ * @retval      #RET_SUCCESSFUL else
+ */
+static returnCode_t GetLinenoFromHKID(pus3HKTable_t *table, pus3HKID_t hkid, length_t *lineno)
+{
+    returnCode_t return_value = RET_NOT_AVAILABLE;
+    pusTableSize_t left       = 0u;
+    pusTableSize_t right      = table->size - 1u;
+    pusTableSize_t cursor     = left + (right - left) / 2u;
+
+    // Perform a binary search
+    while ((left <= right) && (right < table->size) && (return_value != RET_SUCCESSFUL))
+    {
+        if (table->entries[cursor].hkid == hkid)
+        {
+            *lineno      = cursor;
+            return_value = RET_SUCCESSFUL;
+        }
+        else if (table->entries[cursor].hkid < hkid)
+        {
+            left   = cursor + 1u;
+            cursor = left + (right - left) / 2u;
+        }
+        else
+        {
+            right  = cursor - 1u;
+            cursor = left + (right - left) / 2u;
+        }
     }
 
     return return_value;
